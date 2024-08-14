12315f49025b397b761af677a3a5c801772891d0
Simon Fredriksson <simon.fredriksson93@gmail.com>
2024-08-14 14:05:49 +0200

    Third test works

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,10 +1,17 @@
 package com.booleanuk.core;
 
+import java.util.List;
+
 public class Basket {
 
+    private final List<String> bagels;
+
+    public Basket() {
+        this.bagels = List.of("bagel1", "bagel2", "bagel3");
+    }
 
     public boolean addItem(String item) {
-        if (item.isEmpty()) {
+        if (item.isEmpty() || !bagels.contains(item)) {
             return false;
         }
         return true;
