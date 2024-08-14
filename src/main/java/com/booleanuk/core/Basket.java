d8768413102d177e24c06908c3ab04e513b6c140
Simon Fredriksson <simon.fredriksson93@gmail.com>
2024-08-14 14:26:40 +0200

    Refactored Basket

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -5,15 +5,18 @@ import java.util.List;
 public class Basket {
 
     private final List<String> bagels;
+    private List<String> basket;
 
     public Basket() {
         this.bagels = List.of("bagel1", "bagel2", "bagel3", "bagel4");
+        this.basket = List.of();
     }
 
     public boolean addItem(String item) {
         if (item.isEmpty() || !bagels.contains(item)) {
             return false;
         }
+        basket.add(item);
         return true;
     }
 
