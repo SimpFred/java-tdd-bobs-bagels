4b2e6eb293b4586148cf9332de502f915cc22ee0
Simon Fredriksson <simon.fredriksson93@gmail.com>
2024-08-14 14:11:22 +0200

    Fourth Test works

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -7,7 +7,7 @@ public class Basket {
     private final List<String> bagels;
 
     public Basket() {
-        this.bagels = List.of("bagel1", "bagel2", "bagel3");
+        this.bagels = List.of("bagel1", "bagel2", "bagel3", "bagel4");
     }
 
     public boolean addItem(String item) {
